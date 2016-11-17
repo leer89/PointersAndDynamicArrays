cd2be374b08c1eb38dc07a630c4f5de65473024d
leer89 <leer89@gmail.com>
2016-11-17 18:55:37 -0500

    Pointers and Dynamic Arrays

--- PointersAndDynamicArrays/PointersAndDynamicArrays.cpp
+++ PointersAndDynamicArrays/PointersAndDynamicArrays.cpp
@@ -3,9 +3,28 @@
 
 #include "stdafx.h"
 
-
 int main()
 {
+	using namespace std;
+
+	int size, i;
+	int *p, q;
+	cout << "Enter the size of the array: ";
+	Array: "";
+
+	cin >> size;
+	p = new int[size];
+	for (i = 0; i < size; i++)
+		p[i] = i;
+	for (i = 0; i < size; i++)
+		cout << p[i] << endl;
+
+	int *pointer, *pointer2;
+	cout << &size << endl;
+	system("pause");
+
+	p = &i;
+	cout << p;
+
     return 0;
 }
-
